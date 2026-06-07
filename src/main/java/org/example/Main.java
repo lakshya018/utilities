605ca761d83e8e761853b3c0e92368a01fdce54e
lakshya018 <lakshyasoni018@gmail.com>
2026-06-07 21:13:48 +0530

    hello

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,21 @@ import java.util.*;
 
 public class Main {
         static void main() {
-
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the number of elements in the array:");
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            System.out.println("Enter the elements of the array:");
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            Map<Integer, Integer> frequencyMap = new HashMap<>();
+            for (int num : arr) {
+                frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+            }
+            System.out.println("Frequency of each element:");
+            for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
         }
 }
