ba680e5b23b0e7bcee299c9f301920d3a3bb43f3
lakshya018 <lakshyasoni018@gmail.com>
2026-06-07 21:10:13 +0530

    first commit

---
+++ src/main/java/org/example/DesignPrinciples/ObserverPattern.java
@@ -0,0 +1,4 @@
+package org.example.DesignPrinciples;
+
+public class ObserverPattern {
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import jdk.jfr.Frequency;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+        static void main() {
+
+        }
+}
